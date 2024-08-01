fa4b7dc12cb38ce5d40cdd53152e4230ddcd03ea
hyunjin <guswlsdl0121@gmail.com>
2024-08-01 21:21:42 +0900

    fix: ChageType을 정할 때의 전제조건 추가
    - 파일경로가 달라지면서 파일내용이 다른 경우엔 Modified
    - 파일경로만 달라지면 Moved로 구체화
    - Change 객체의 calType 메서드 참고

--- src/main/kotlin/com/github/guswlsdl0121/messagemaker/services/diff/generator/SimpleDiffGenerator.kt
+++ src/main/kotlin/com/github/guswlsdl0121/messagemaker/services/diff/generator/SimpleDiffGenerator.kt
@@ -5,44 +5,27 @@ class SimpleDiffGenerator : DiffGenerator {
         val beforeLines = beforeContent.lines()
         val afterLines = afterContent.lines()
 
-        val diff = mutableListOf<String>()
-        var i = 0
-        var j = 0
+        return buildString {
+            var i = 0
+            var j = 0
 
-        while (i < beforeLines.size || j < afterLines.size) {
-            when {
-                i >= beforeLines.size -> {
-                    // 원본 내용이 모두 처리되었고, 새 내용에 추가된 줄들을 처리
-                    diff.add("+${afterLines[j]}")
-                    j++
-                }
-
-                j >= afterLines.size -> {
-                    // 새 내용이 모두 처리되었고, 원본 내용에서 삭제된 줄들을 처리
-                    diff.add("-${beforeLines[i]}")
-                    i++
-                }
-
-                beforeLines[i] == afterLines[j] -> {
-                    // 양쪽 내용이 동일한 경우, 변경 없이 다음 줄로 넘어감
-                    i++
-                    j++
-                }
-
-                else -> {
-                    // 내용이 다른 경우, 삭제와 추가로 처리
-                    if (beforeLines[i].isNotEmpty()) {
-                        diff.add("-${beforeLines[i]}")
+            while (i < beforeLines.size || j < afterLines.size) {
+                when {
+                    i >= beforeLines.size -> append("+${afterLines[j++]}\n")
+                    j >= afterLines.size -> append("-${beforeLines[i++]}\n")
+                    beforeLines[i] == afterLines[j] -> {
+                        i++
+                        j++
                     }
-                    if (afterLines[j].isNotEmpty()) {
-                        diff.add("+${afterLines[j]}")
+
+                    else -> {
+                        if (beforeLines[i].isNotEmpty()) append("-${beforeLines[i]}\n")
+                        if (afterLines[j].isNotEmpty()) append("+${afterLines[j]}\n")
+                        i++
+                        j++
                     }
-                    i++
-                    j++
                 }
             }
-        }
-
-        return diff.joinToString("\n")
+        }.trimEnd()
     }
 }
